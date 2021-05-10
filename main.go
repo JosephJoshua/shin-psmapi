a3c71ca78da59979aa6e2ccc7a33e3853f27a232
JosephJoshua <jj.anggita@gmail.com>
2021-05-10 10:04:22 +0900

    Add gin dependency

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "3030"
+	}
+
+	r.Run(":" + port)
+}
